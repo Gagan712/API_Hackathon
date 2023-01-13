4ff77bb4e3f1a9db8a87e8a978862db980dc4f51
gagan712 <saini.gaganpreet@gmail.com>
2023-01-13 15:28:57 -0800

    adding Post method

---
+++ src/test/java/testRunner/Runner.java
@@ -0,0 +1,24 @@
+package testRunner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features ={"src/test/resources/Features"},
+		glue="stepDefinition",
+		dryRun =false,
+		monochrome = false,
+        plugin= {"pretty","html:target/save.html"}
+		
+		)
+	
+	
+	
+	
+
+
+public class Runner {
+
+}
